bed20fc905fe743bb994a8d6a856ebc8943b3a8d
fanani <fananiapps@gmail.com>
2020-10-15 19:28:51 -0700

    with other lappy

---
+++ build/precache-manifest.31a876e9da8c18440bfa6483ed8ed858.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84d12aa1e716caf4082007be6b25852",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11d7e8d874df347f2fb4",
+    "url": "/static/css/main.24d32881.chunk.css"
+  },
+  {
+    "revision": "19f77bc56bc75f5d268c",
+    "url": "/static/js/2.f58d5151.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f58d5151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11d7e8d874df347f2fb4",
+    "url": "/static/js/main.5fa909a6.chunk.js"
+  },
+  {
+    "revision": "a7d0432c6ae3dc2cdebd",
+    "url": "/static/js/runtime-main.b0d63ec5.js"
+  },
+  {
+    "revision": "27a126f763671442f78500bddf21159e",
+    "url": "/static/media/bg.27a126f7.jpg"
+  },
+  {
+    "revision": "3b79cbe50fd795b66422b38e0504cd68",
+    "url": "/static/media/bullhorn.3b79cbe5.png"
+  },
+  {
+    "revision": "b462b7540915fb146947f32ef8f15589",
+    "url": "/static/media/kemenag.b462b754.png"
+  }
+]);
--- src/components/Signin.js
+++ src/components/Signin.js
@@ -6,7 +6,7 @@ import logo from '../assets/img/kemenag.png'
 export default class Signin extends Component {
     render() {
         return (
-            <div className="container-1">
+            <div className="container">
                  <div className="section">
                   <div className="row">
                     <div className="col s12 m6">
@@ -14,8 +14,8 @@ export default class Signin extends Component {
                         <div className="content-info">
                           <div className="img-style">
                             <img src={bullhorn} className="ic-info"/>
+                            Pengumuman!
                           </div>
-                          Pengumuman!
                         </div>
                         <div>
                           <p>Tanggal Pendataan 2020-02-10 s/d 2020-05-31 untuk jenjang MA, MTS, MI, RA</p>
@@ -29,17 +29,17 @@ export default class Signin extends Component {
                         </div>
                         <h3>Emis Madrasah</h3>
                         <p>Silakan masukan Nama Pengguna dan Kata Sandi</p>
-                        <div className="input-field col s6">
+                        <div className="input-field">
                           <i className="material-icons prefix">account_circle</i>
                           <input id="icon_prefix" type="text" className="validate"/>
                           <label for="icon_prefix">Nama Pengguna</label>
                         </div>
-                        <div className="input-field col s6">
+                        <div className="input-field">
                           <i className="material-icons prefix">lock_outline</i>
                           <input id="icon_prefix" type="text" class="validate"/>
                           <label for="icon_prefix">Kata Sandi</label>
                         </div>
-                        <div className="input-field col s6">
+                        <div className="input-field">
                           <button className="btn waves-effect btn-masuk waves-light blue" type="submit" name="action">MASUK
                             <i className="material-icons right">send</i>
                           </button>
